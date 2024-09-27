4c749f2ab65df6e6d4976c9c920fc4a9290f48ed
corey <corey.grant.durham@hotmail.co.uk>
2024-09-27 23:16:59 +0100

    constructed redis url

--- server/redisStore.js
+++ server/redisStore.js
@@ -3,18 +3,18 @@ const {createClient} = require("redis")
 const redisUrl = process.env["osltl_redis_connectionstring"]
 const redisUsername = process.env["osltl_redis_username"]
 const redisPassword = process.env["osltl_redis_password"]
-const redisPort = process.env["osltl_redis_port"]
+const redisPort = process.env["osltl_redis_port"] || 6379;
 // Initialize client.
 console.log("redis")
 console.log(redisUsername)
 console.log(redisUrl)
 console.log(redisPassword)
 console.log(redisPort);
-let redisClient = createClient(redisPort, redisUrl, {
-    host: redisUrl,
-    port: redisPort,
-    password: redisPassword
-})
+const url = `redis://${redisUsername}:${redisPassword}@${redisUrl}:${redisPort}`;
+console.log(url);
+let redisClient = createClient({
+    url: url
+});
 redisClient.connect().catch(console.error)
 
 // Initialize store.
